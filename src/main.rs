62c6b2c7a6787bfa89a8722c9187ba307c4360da
Andrei <andrei.evguenov@gmail.com>
2022-06-10 23:35:20 +1200

    Second

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Initial - 1");
+use std::time::Duration;
+
+use tokio::time::sleep;
+
+#[tokio::main]
+async fn main() {
+    sleep(Duration::from_secs(10)).await;
+    println!("2");
 }
