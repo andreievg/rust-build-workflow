35699d4b378d7e35c42817496aa74af54118a3c0
Andrei <andrei.evguenov@gmail.com>
2022-06-14 18:46:07 +1200

    check openssl

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use std::time::Duration;
-
-use tokio::time::sleep;
+use openssl::ssl::{SslConnector, SslMethod};
+use std::io::{Read, Write};
+use std::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
-    sleep(Duration::from_secs(10)).await;
-    println!("5");
+    use openssl::ssl::{SslConnector, SslMethod};
+    use std::io::{Read, Write};
+    use std::net::TcpStream;
+
+    let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
+
+    let stream = TcpStream::connect("google.com:443").unwrap();
+    let mut stream = connector.connect("google.com", stream).unwrap();
+
+    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
+    let mut res = vec![];
+    stream.read_to_end(&mut res).unwrap();
+    println!("{}", String::from_utf8_lossy(&res));
 }
