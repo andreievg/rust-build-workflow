dc5d274a69504d3e6e1be71a7dd17c3b349c425d
Andrei <andrei.evguenov@gmail.com>
2022-06-18 22:23:31 +1200

    remove openssl

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,18 +1,6 @@
 #[tokio::main]
 async fn main() {
-    use openssl::ssl::{SslConnector, SslMethod};
-    use std::io::{Read, Write};
-    use std::net::TcpStream;
-
-    let connector = SslConnector::builder(SslMethod::tls()).unwrap().build();
-
-    let stream = TcpStream::connect("google.com:443").unwrap();
-    let mut stream = connector.connect("google.com", stream).unwrap();
-
-    stream.write_all(b"GET / HTTP/1.0\r\n\r\n").unwrap();
-    let mut res = vec![];
-    stream.read_to_end(&mut res).unwrap();
-    println!("{}", String::from_utf8_lossy(&res));
+    println!("Check");
 }
 
 #[cfg(test)]
